a9d5a4dbd77beac479c549d3e78eccfaea1e4d21
Jason Miller <jason@developit.ca>
2015-11-02 22:58:39 -0500

    More readable homepage

--- src/components/main.js
+++ src/components/main.js
@@ -31,27 +31,30 @@ export default ({ error, docs, symbol }) => {
 				</Card.Title>
 
 				<Card.Text>
-					<Icon icon="info" style="float:left; margin-top:30px" />
+					<Icon icon="info" style="float:left; margin-top:20px" />
 					<p class="info">
-						Paste your .json URL into the box above
-						to load and render your documentation.
+						Paste your <code>JSON</code> documentation URL into the box above to get started.
 					</p>
 
 					<ul style="list-style:none;padding:0 10px;">
 						<li>
 							<Icon icon="build" style="padding:10px; vertical-align:middle" />
-							Supports output from <a href="http://documentation.js.org" target="_blank">documentation.js</a>.
+							Renders <code>JSON</code> output from <a href="http://documentation.js.org" target="_blank">documentation.js</a>
+						</li>
+						<li>
+							<Icon icon="link" style="padding:10px; vertical-align:middle" />
+							Hotlink from Github &amp; Gist (even <a href="https://gist.github.com/developit/04ad7ac878b5fa35e76e" target="_blank">automatically</a>!)
 						</li>
 						<li>
 							<Icon icon="code" style="padding:10px; vertical-align:middle" />
-							<a href="https://github.com/developit/documentation-viewer" target="_blank">Documentation Viewer project on Github</a>
+							Open Source: <a href="https://github.com/developit/documentation-viewer" target="_blank">add your must-have feature</a>
 						</li>
 					</ul>
 				</Card.Text>
 
-				<Card.Actions style="text-align:center;">
-					<small>Powered by &nbsp;</small>
-					<a href="https://github.com/developit/preact" target="_blank">Preact</a>
+				<Card.Actions style="text-align:center; color:#888; font-size:80%;">
+					Made with <Icon icon="favorite" style="vertical-align:middle" /> by <a href="https://github.com/developit" target="_blank">developit</a>.
+					Built with <a href="https://github.com/developit/preact" target="_blank">Preact</a>.
 				</Card.Actions>
 			</Card>
 		);
